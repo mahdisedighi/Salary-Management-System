4b23767d44cff48ba0199a9499a210831116dcf3
Mahdi Saremi <mahdisarmi00@gmail.com>
2024-12-26 20:11:32 +0330

    updated base template

--- test.sql
+++ test.sql
@@ -1,6 +1,17 @@
-select * from saeed
+-- Mahdi Saremi
+CREATE TABLE users
+(
+    id   INT,
+    name VARCHAR(255)
+);
 
 
+-- Sedighi Homeless
+DELETE
+FROM user_homes
+WHERE user_name = 'Sedighi';
 
 
-dsfsddsf
+-- Saeed safaee
+UPDATE user_profile
+SET remaining_courses = remaining_courses + 1;
